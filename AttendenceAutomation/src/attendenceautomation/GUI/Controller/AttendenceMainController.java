742fd98fd977294bcd84db68216129c1135ca0ed
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-02-01 12:48:08 +0100

    Basic structure

--- AttendenceAutomation/src/attendenceautomation/GUI/Controller/AttendenceMainController.java
+++ AttendenceAutomation/src/attendenceautomation/GUI/Controller/AttendenceMainController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package attendenceautomation;
+package attendenceautomation.GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author Bjarne Helsinghof
  */
-public class FXMLDocumentController implements Initializable
+public class AttendenceMainController implements Initializable
 {
     
     @FXML

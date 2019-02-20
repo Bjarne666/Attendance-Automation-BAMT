f8f629785479e7d3b5f64cddbb2251ac2832c983
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-02-20 19:39:45 +0100

    Attendancemainviewthingy

--- AttendenceAutomation/src/attendenceautomation/GUI/Controller/AttendenceStudentViewController.java
+++ AttendenceAutomation/src/attendenceautomation/GUI/Controller/AttendenceStudentViewController.java
@@ -11,7 +11,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
@@ -19,49 +18,54 @@ import javafx.scene.control.ToggleGroup;
 /**
  * FXML Controller class
  *
- * @author Asvør
+ * @author Kokushi
  */
 public class AttendenceStudentViewController implements Initializable {
 
     @FXML
     private Label lblUser;
     @FXML
-    private RadioButton rdBtnAbsent;
-    @FXML
-    private RadioButton rdBtnPresent;
+    private ToggleGroup attendance;
     @FXML
     private Label lblAttendance;
-    @FXML
-    private ToggleGroup attendance;
     @FXML
     private Label lblDate;
     @FXML
     private Label lblName;
     @FXML
     private Label lblEducation;
-
-    public AttendenceStudentViewController() {
-        lblAttendance.setVisible(false);
-    }
+    @FXML
+    private RadioButton rdBtnAbsent;
+    @FXML
+    private RadioButton rdBtnPresent;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-    }
+        lblUser.setText("");
+        lblDate.setText("");
+        lblEducation.setText("");
+        lblName.setText("");
+        lblAttendance.setVisible(false);
+    }    
 
     @FXML
     private void handleAttendance(ActionEvent event) {
         if (attendance.getSelectedToggle() == rdBtnPresent) {
             lblAttendance.setText("Present");
-        } else if (attendance.getSelectedToggle() == rdBtnAbsent) {
-            lblAttendance.setText("Absent");
+            lblAttendance.setVisible(true);
+            return;
         }
-        Alert alert = new Alert(Alert.AlertType.ERROR, "You have to select either absent or present", ButtonType.OK);
-        alert.showAndWait();
-        return;
+        else if (attendance.getSelectedToggle() == rdBtnAbsent) {
+            lblAttendance.setText("Absent");
+            lblAttendance.setVisible(true);
+            return;
+        }
+         Alert alert = new Alert(Alert.AlertType.ERROR, "You have to select either present or absent");
+            alert.showAndWait();
+        
     }
-
+    
 }

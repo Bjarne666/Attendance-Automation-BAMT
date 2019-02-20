d090bac189822c2dc7d1f09485e687ad324551f4
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-02-20 19:11:31 +0100

    studentStuff

--- AttendenceAutomation/src/attendenceautomation/GUI/Controller/AttendenceStudentViewController.java
+++ AttendenceAutomation/src/attendenceautomation/GUI/Controller/AttendenceStudentViewController.java
@@ -7,23 +7,61 @@ package attendenceautomation.GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
  *
  * @author Asvør
  */
-public class AttendenceStudentViewController implements Initializable
-{
+public class AttendenceStudentViewController implements Initializable {
+
+    @FXML
+    private Label lblUser;
+    @FXML
+    private RadioButton rdBtnAbsent;
+    @FXML
+    private RadioButton rdBtnPresent;
+    @FXML
+    private Label lblAttendance;
+    @FXML
+    private ToggleGroup attendance;
+    @FXML
+    private Label lblDate;
+    @FXML
+    private Label lblName;
+    @FXML
+    private Label lblEducation;
+
+    public AttendenceStudentViewController() {
+        lblAttendance.setVisible(false);
+    }
 
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
-        // TODO
-    }    
-    
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    private void handleAttendance(ActionEvent event) {
+        if (attendance.getSelectedToggle() == rdBtnPresent) {
+            lblAttendance.setText("Present");
+        } else if (attendance.getSelectedToggle() == rdBtnAbsent) {
+            lblAttendance.setText("Absent");
+        }
+        Alert alert = new Alert(Alert.AlertType.ERROR, "You have to select either absent or present", ButtonType.OK);
+        alert.showAndWait();
+        return;
+    }
+
 }
